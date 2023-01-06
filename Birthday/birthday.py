3df6c4ac905d7d3eb1d6f8b5a89fb2bbdb0d8546
Konrad P <kokiziom@gmail.com>
2023-01-06 18:14:03 +0100

    file with birthdays

--- Birthday/birthday.py
+++ Birthday/birthday.py
@@ -1,35 +1,40 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+import json
 
-birthday = {
-    'Kasia':'01-01-2023',
-    'Jan':'27-10-1990',
-    'Pankracy':'05-07-1967'
-}
+try:
+    with open('calendar.json', 'r+') as file:
+        birthday = json.load(file)
+except:
+    birthday = {}
 
 menu = '''
     What you want to do?
     1 - check birthdays
+    2 - add person
     0 - close the program
 '''
 
 print(menu)
 today = datetime.now()
-action = input('Input action number:')
 while True:
     
-
+    action = input('Input action number:')
     if action == '1':
-        for person in birthday:
-            birth_day = datetime.strptime(birthday[person], '%d-%m-%Y')
+        for person, date in birthday.items():
+            birth_day = datetime.strptime(date, '%d-%m-%Y')
             birth_day = birth_day.replace(year = today.year)
-            print(birth_day)
             delta = birth_day - today
             if delta.days < 0:
                 birth_day = birth_day.replace(year = today.year + 1)
                 delta = birth_day - today
-            print(f'{person} {birthday[person]}, days to birthday: {delta.days}')
-        break
+            print(f'{person} {date}, days to birthday: {delta.days}')
     elif action == '0':
+        with open('calendar.json', 'w+') as file:
+            json.dump(birthday, file)
         break
+    elif action == '2':
+        new_name = input('Input name:')
+        new_date = input('Input date in format dd-mm-yyyy:')
+        birthday[new_name] = new_date
     else:
         action = input('Input proper action number:')
