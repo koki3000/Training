9fd082efb9cc9d42b73b7fda51b477dca111e544
Konrad P <kokiziom@gmail.com>
2023-01-14 19:20:49 +0100

    added new class ToDoList

--- TodoList/list.py
+++ TodoList/list.py
@@ -11,13 +11,33 @@ class Task():
 def prio_sort(task):
     return task.prio
 
-def show_list(list):
-    list.sort(reverse=True, key=prio_sort)
-    cnt = 1
-    print('\nTasks:')
-    for task in list:
-        print(f'{cnt} - {task}')
-        cnt += 1
+
+class ToDoList:
+    list = []
+    def __init__(self, list):
+        self.list = list
+    
+    def append_task(self, desc, prio):        
+        new_task = Task(desc, int(prio))
+        self.list.append(new_task)
+    
+    def add_task(self):
+        desc = input('Please input task description: ')
+        prio = input('Please input task priority: ')
+        self.append_task(desc, prio)
+        
+    def delete_task(self):
+        self.list.sort(reverse=True, key=prio_sort)
+        delete = input('Which task do you want to delete: ')
+        del self.list[int(delete)-1]
+
+    def show_list(self):
+        self.list.sort(reverse=True, key=prio_sort)
+        cnt = 1
+        print('\nTasks:')
+        for task in self.list:
+            print(f'{cnt} - {task}')
+            cnt += 1
         
 if __name__ == '__main__':
     
@@ -33,19 +53,16 @@ Please select option: """
     test1 = Task('pierwsze', 100)
     test2 = Task('drugie', 50)
     test3 = Task('najwazniejsze', 500)
-    list = [test1, test2, test3]
+    test_list = [test1, test2, test3]
+    list = ToDoList(test_list)
 
     while option != '0':
+        
         option = input(menu)
         if option == '1':
-            desc = input('Please input task description: ')
-            prio = input('Please input task priority: ')
-            new_task = Task(desc, int(prio))
-            print(new_task)
-            list.append(new_task)
+            list.add_task()
         elif option == '2':
-            show_list(list)
-            delete = input('Which task do you want to delete: ')
-            del list[int(delete)-1]
+            list.show_list()
+            list.delete_task()
         elif option == '3':
-            show_list(list)
+            list.show_list()
